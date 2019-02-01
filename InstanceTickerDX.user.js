7b98baf9c3774cc9b87b92e12764f688d49475fb
weep <weep@live.jp>
2019-02-02 05:59:15 +0900

    #InstanceTicker (DX) Ver.201902020002.user.js

--- InstanceTickerDX.user.js
+++ InstanceTickerDX.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         #InstanceTickerMisskey.user.js
+// @name         #InstanceTicker (DX) user.js
 // @namespace    https://github.com/weepjp/InstanceTicker
-// @version      201902020001
-// @description  #InstanceTicker とは、Mastodon タイムライン上の投稿者のみなさんに所属インスタンス名を彩るカスタムCSSです。
-// @author       weep https://weep.me/@weep
+// @version      201902020002
+// @description  #InstanceTicker DX は、 Mastodon と Misskey タイムライン上の投稿者のみなさんに所属インスタンス名を彩るカスタムCSSです。
+// @author       weep https://weep.me/@weep (Special Thanks: popn_ja)
 //
 // @license      Original CSS © https://odakyu.app/@ars42525 and #InstanceTicker © 2018 https://weep.me/@weep Released under the MIT license https://opensource.org/licenses/MIT
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABG0lEQVQ4y6WTLWzCUBSFnwAxxMQEE5uYQMxMDIGYmamEjvCX2lVM0ASxpCOBZE1mSINA1CCqq6ura6urq6urz3oeKEiBPF7y2S/v3HuuCMMQURRJ4jhGkiSSNE2RZZkkz3NJURQ4fCIIAtQnWzQmHm7HG9yN1mgOV3gY/uFp4KDVX+K5P8fLxzfa+gyd3hRv3S+8dz93At/3USsFN+MDwYCC31KwKAU/UvBaJagrChhJeJ53ncB13TOCZaWAgxWO4xwJ7s8JejsBN3SVgKsWtm0rR5ACy7KUh8jCCdM0lSOwuRcIqn/A+gvDMC6YwbxaoOu6cpV5iELTtOMIoxUeTx3TfgY8RCkg/AlhJM6FcENcM2Ff2FrC+vOGyD8Yvit5VS8OawAAAABJRU5ErkJggg==
