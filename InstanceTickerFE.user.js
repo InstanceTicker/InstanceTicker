ca5c5baea60b3155573a2125b0fed55faf454d74
weep <weep@live.jp>
2019-03-30 03:04:00 +0900

    ⚠

--- InstanceTickerFE.user.js
+++ InstanceTickerFE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         #InstanceTickerFE user.js
 // @namespace    https://github.com/fedpla/InstanceTicker/
-// @version      201903200006
+// @version      201903290537
 // @description  #InstanceTickerFE は、Pleroma の Mastodon FE タイムライン上投稿者に所属インスタンス名を彩るカスタムCSSです。
 // @author       weep https://weep.me/@weep (Special Thanks: popn_ja)
 //
@@ -62,7 +62,7 @@
 //
 //
 /*****************
- * ふぇ・・・　　 *
+ * ふぇ～・・・　　 *
  ****************/
 //
 //
